e5833f1d73bfff28b1486a2bdc9767ce3490c775
TuanVuTrinh <vutuantrinh2000@gmail.com>
2024-07-31 17:40:21 +0700

    x

--- scripts/remove_silence.py
+++ scripts/remove_silence.py
@@ -21,7 +21,7 @@ MODEL, (get_speech_timestamps, _, silero_read_audio, _, _) = torch.hub.load(
 MODEL = MODEL.to("cuda")
 
 
-def vad_filter(infile: str, outfile: str, split_threshold, max_threshold) -> None:
+def vad_filter(infile: str, outfile: str, split_threshold=15*60, max_threshold=20*60) -> None:
 	wav = silero_read_audio(infile, sampling_rate=SAMPLING_RATE).to("cuda")  # SileroVAD operate on mono channel at 16 kHz
 	with torch.inference_mode(), tqdm(total=wav.shape[0], bar_format=TQDM_PBAR_FORM) as pbar:
 		speech_timestamps: list[dict[str, int]] = get_speech_timestamps(
